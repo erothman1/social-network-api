7a3012e1820b212ccaf627db1043d9fac9ac6986
Elva Rothman <erothman@tulane.edu>
2023-05-13 17:28:37 -0400

    wrote userControllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,98 @@
-const { response } = require("express")
-const User = require("../models/User")
+const { User, Thought } = require("../models")
 
 module.exports = {
     //get all users
     async getUsers(req, res) {
         try {
-            response = await User.find({})
+            const response = await User.find({})
             res.status(200).json(response)
         } catch (err) {
-            res.status(500).json({ message: err})
+            res.status(500).json({ message: err })
         }
     },
     //get single user by its userId
     async getSingleUser(req, res) {
         try {
-            response = await User.findOne({ _id: req.params.userId }).select("-__v").populate("thought")
+            const response = await User.findOne({ _id: req.params.userId })
+                .select("-__v")
+                .populate("thoughts", "friends")
+
             !response ? res.status(404).json({ message: 'No user with that ID' }) : res.json(response)
+
         } catch (err) {
-            res.status(500).json({ message: err})
+            res.status(500).json({ message: err })
         }
     },
     //create a new user 
     async createUser(req, res) {
-        try{
-            response = await User.create(req.body) 
+        try {
+            const response = await User.create(req.body)
             res.json(response)
         } catch (err) {
-            res.status(500).json({ message: err})
+            res.status(500).json({ message: err })
+        }
+    },
+    //update a user
+    async updateUser(req, res) {
+        try {
+            const response = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            )
+
+            !response ? res.status(404).json({ message: "No user with that ID" }) : res.json(response)
+        } catch (err) {
+            res.status(500).json({ message: err })
+        }
+    },
+    //delete a user and their associated thoughts
+    async updateUser(req, res) {
+        try {
+            const userData = await User.findOneAndRemove({ _id: req.params.userId })
+
+            if (!userData) {
+                return res.status(404).json({ message: 'No user with that ID' }) 
+            }
+
+            const thoughtData = await Thought.deleteMany(
+                { username: req.params.userId }
+            )
+
+            !thoughtData 
+            ? res.status(404).json({ message: "User deleted but no thoughts found" }) 
+            : res.json("User successfully deleted")
+
+        } catch (err) {
+            res.status(500).json({ message: err })
+        }
+    },
+    //add new friend to user's friend list
+    async newFriend(req, res) {
+        try {
+            const response = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            )
+
+            !response ? res.status(404).json({ message: "No user with that ID" }) : res.json(response)
+        } catch (err) {
+            res.status(500).json({ message: err })
+        }
+    },
+    //remove friend from user's friend list
+    async deleteFriend(req, res) {
+        try {
+            const response = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            )
+
+            !response ? res.status(404).json({ message: "No user with that ID" }) : res.json(response)
+        } catch (err) {
+            res.status(500).json({ message: err })
         }
     }
 }
