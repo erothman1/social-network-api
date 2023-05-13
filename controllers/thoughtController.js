3ba30ec77eae489b017fa0fcb0a9747b06dafbb9
Elva Rothman <erothman@tulane.edu>
2023-05-13 18:46:39 -0400

    edited thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -13,7 +13,7 @@ module.exports = {
     //get single thought 
     async getSingleThought(req, res) {
         try {
-            const response = await Thought.findOne({ _id: req.body.userId })
+            const response = await Thought.findOne({ _id: req.body.thoughtId })
                 .select("-__v")
 
             !response ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(response)
